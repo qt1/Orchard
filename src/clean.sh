f1cd65e4413a79cc12c3b729d456a459397d6e47
qt1 <baruch@ibn-labs.com>
2015-09-08 22:34:24 +0300

    clueanup bash script

---
+++ src/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf Orchard.Web/Core/obj Orchard.Web/Themes/obj Orchard.Web/Core/bin Orchard.Web/Themes/bin
+rm -rf Orchard.Web/*/*/{obj,bin}
+rm Orchard.Web/App_Data/Dependencies/*
+rm -rf 'C:\Users\baruch\AppData\Local\Temp\Temporary ASP.NET Files'
