45fb3a6a045ba574f1673298eb5bd5016a72486d
qt1 <baruch@ibn-labs.com>
2015-09-10 04:38:45 +0300

    ignore cr

--- src/clean.sh
+++ src/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#
+(set -o igncr) 2>/dev/null && set -o igncr; # this comment is needed
+
 rm -rf Orchard.Web/Core/obj Orchard.Web/Themes/obj Orchard.Web/Core/bin Orchard.Web/Themes/bin
 rm -rf Orchard.Web/*/*/{obj,bin}
 rm Orchard.Web/App_Data/Dependencies/*
