36542dd58a24b9543933c7810da59165b28fed2e
qt1 <baruch@ibn-labs.com>
2015-09-10 03:35:35 +0300

    merge sln, update sh

--- src/clean.sh
+++ src/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 rm -rf Orchard.Web/Core/obj Orchard.Web/Themes/obj Orchard.Web/Core/bin Orchard.Web/Themes/bin
 rm -rf Orchard.Web/*/*/{obj,bin}
 rm Orchard.Web/App_Data/Dependencies/*
