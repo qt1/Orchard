890f4edd1dd6e328d042bfd9dfe026f845a95910
Baruch Nissenbaum <baruch@ibn-labs.com>
2015-10-25 21:45:53 +0200

    search for cygwin in alternative locations

--- src/clean.cmd
+++ src/clean.cmd
@@ -1,4 +1,18 @@
-set cygwindir=C:\Cygwin64
+FOR %%b in ( 
+       "C:\Cygwin64"
+       "D:\Cygwin64"
+       "C:\Cygwin"
+       "C:\Cygwin"
+    ) do (
+    if exist %%b ( 
+       set cygwindir=%%b
+       goto exec
+    )
+)
+  
+echo "Unable to find cygwin"
+
+:exec
 
 %CygwinDir%\bin\bash --login -c "cd `cygpath '%CD%'` && bash clean.sh "
 
